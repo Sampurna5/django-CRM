fd53fc9c5e0545dfba24d51d1307d09006bf0d54
Sampurna5 <msampurna05@gmail.com>
2021-10-19 08:32:09 +0545

    Password reset

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -26,21 +26,19 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from leads.views import HomePageView, SignUpView
-from leads.views import landing_page
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePageView.as_view(), name='landing-page'),
     path('leads/', include('leads.urls', namespace='leads')),
     path('agents/', include('agents.urls', namespace='agents')),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
     path('password-reset-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('password-reset-complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    path('signup/', SignUpView.as_view(), name='signup'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout')
 ]
 
 if settings.DEBUG:
